31f7c9305ef491544d5afcea9126152fdecea262
pablo carbonell <p.carbonell@innovation.es>
2023-11-15 16:59:11 +0100

    Genera barcos de tres casillas, pero a veces estan uno al lado del otro

--- src/components/TableroRandom.vue
+++ src/components/TableroRandom.vue
@@ -36,9 +36,9 @@
     while (cuadros.length < cantidad) {
         var rowIndex = Math.floor(Math.random() * 10);
         var colIndex = Math.floor(Math.random() * 10);
-        
         const direction = Math.random() < 0.5 ? "right" : "down";
         const size = 1
+
         if (verificaIndices(rowIndex, colIndex, arrayIndex.value)){
             cuadros.push({rowIndex, colIndex, direction, size})
             arrayIndex.value.push({rowIndex, colIndex, direction, size})
@@ -49,7 +49,7 @@
 
     console.log(arrayIndex.value)
 
-    const generarFiguras = (cantidad: number): {rowIndex: number, colIndex: number, direction: string, size: number}[] => {
+    const generarFiguras2 = (cantidad: number): {rowIndex: number, colIndex: number, direction: string, size: number}[] => {
     const figuras: {rowIndex: number, colIndex: number, direction: string, size: number}[] = [];
     while (figuras.length < cantidad) {
         var rowIndex = Math.floor(Math.random() * 10);
@@ -65,7 +65,7 @@
         } else {
             rowIndex = Math.floor(Math.random() * 9);
             colIndex = Math.floor(Math.random() * 10);
-            var verificaLado: boolean = verificaIndices(rowIndex+1, colIndex, arrayIndex.value)
+            var verificaLado: boolean = verificaIndices(rowIndex +1, colIndex, arrayIndex.value)
         }
         
         if (verificaIndices(rowIndex, colIndex, arrayIndex.value) || verificaLado){
@@ -78,6 +78,107 @@
                 arrayIndex.value.push({rowIndex, colIndex, direction, size})
             } else {
                 rowIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+            }
+        
+    }}
+    return figuras;
+}
+
+const generarFiguras3 = (cantidad: number): {rowIndex: number, colIndex: number, direction: string, size: number}[] => {
+    const figuras: {rowIndex: number, colIndex: number, direction: string, size: number}[] = [];
+    while (figuras.length < cantidad) {
+        var rowIndex = Math.floor(Math.random() * 10);
+        var colIndex = Math.floor(Math.random() * 10);
+        
+        const direction = Math.random() < 0.5 ? "right" : "down";
+        const size = 3;
+
+        if (direction == "right"){
+            rowIndex = Math.floor(Math.random() * 10);
+            colIndex = Math.floor(Math.random() * 8);
+            
+            const celda1 = verificaIndices(rowIndex, colIndex+1, arrayIndex.value)
+            const celda2 = verificaIndices(rowIndex, colIndex+2, arrayIndex.value)
+            
+            var verificaLado: boolean = 
+            celda1 && celda2
+        } else {
+            rowIndex = Math.floor(Math.random() * 8);
+            colIndex = Math.floor(Math.random() * 10);
+            
+            const celda1 = verificaIndices(rowIndex+1, colIndex, arrayIndex.value)
+            const celda2 = verificaIndices(rowIndex+2, colIndex+2, arrayIndex.value)
+
+            var verificaLado: boolean = 
+            verificaIndices(rowIndex+1, colIndex, arrayIndex.value) &&
+            verificaIndices(rowIndex+2, colIndex, arrayIndex.value)
+        }
+        
+        if (verificaIndices(rowIndex, colIndex, arrayIndex.value) || verificaLado){
+
+            arrayIndex.value.push({rowIndex, colIndex, direction, size})
+            figuras.push({rowIndex, colIndex, direction, size})
+
+            if (direction === "right"){
+                colIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+                colIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+            } else {
+                rowIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+                rowIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+            }
+        
+    }}
+    return figuras;
+}
+
+const generarFiguras4 = (cantidad: number): {rowIndex: number, colIndex: number, direction: string, size: number}[] => {
+    const figuras: {rowIndex: number, colIndex: number, direction: string, size: number}[] = [];
+    while (figuras.length < cantidad) {
+        var rowIndex = Math.floor(Math.random() * 10);
+        var colIndex = Math.floor(Math.random() * 10);
+        
+        const direction = Math.random() < 0.5 ? "right" : "down";
+        const size = 4;
+
+        if (direction == "right"){
+            rowIndex = Math.floor(Math.random() * 10);
+            colIndex = Math.floor(Math.random() * 7);
+            var verificaLado: boolean = 
+            verificaIndices(rowIndex, colIndex+1, arrayIndex.value) &&
+            verificaIndices(rowIndex, colIndex+2, arrayIndex.value) &&
+            verificaIndices(rowIndex, colIndex+3, arrayIndex.value)
+        } else {
+            rowIndex = Math.floor(Math.random() * 7);
+            colIndex = Math.floor(Math.random() * 10);
+            var verificaLado: boolean = 
+            verificaIndices(rowIndex+1, colIndex, arrayIndex.value) &&
+            verificaIndices(rowIndex+2, colIndex, arrayIndex.value) &&
+            verificaIndices(rowIndex+3, colIndex, arrayIndex.value)
+        }
+        
+        if (verificaIndices(rowIndex, colIndex, arrayIndex.value) || verificaLado){
+
+            arrayIndex.value.push({rowIndex, colIndex, direction, size})
+            figuras.push({rowIndex, colIndex, direction, size})
+
+            if (direction === "right"){
+                colIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+                colIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+                colIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+            } else {
+                rowIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+                rowIndex++;
+                arrayIndex.value.push({rowIndex, colIndex, direction, size})
+                colIndex++;
                 arrayIndex.value.push({rowIndex, colIndex, direction, size})
             }
         
@@ -105,10 +206,11 @@ const verificaIndices = (rowIndexTest: number, colIndexTest: number, arrayIndex:
 
 const asignarClaseOcupado = () => {
     
-    const figuras = generarFiguras(3);
+    const figuras3 = generarFiguras3(2);
+    const figuras = generarFiguras2(3);
     const cuadros = generarCuadros(4);
-    
-    const elementos = figuras.concat(cuadros);
+
+    const elementos = figuras3.concat(figuras.concat(cuadros));
 
     elementos.forEach(elemento => {
         const {rowIndex, colIndex, direction, size} = elemento;
@@ -119,6 +221,24 @@ const asignarClaseOcupado = () => {
             } 
             if (direction === "down" && size === 2) {
                 tabla.value[rowIndex + 1][colIndex] = "ocupado";
+            }
+            if (direction === "right" && size === 3){
+                tabla.value[rowIndex][colIndex + 1] = "ocupado";
+                tabla.value[rowIndex][colIndex + 2] = "ocupado";
+            } 
+            if (direction === "down" && size === 3) {
+                tabla.value[rowIndex + 1][colIndex] = "ocupado";
+                tabla.value[rowIndex + 2][colIndex] = "ocupado";
+            }
+            if (direction === "right" && size === 4){
+                tabla.value[rowIndex][colIndex + 1] = "ocupado";
+                tabla.value[rowIndex][colIndex + 2] = "ocupado";
+                tabla.value[rowIndex][colIndex + 3] = "ocupado";
+            } 
+            if (direction === "down" && size === 4) {
+                tabla.value[rowIndex + 1][colIndex] = "ocupado";
+                tabla.value[rowIndex + 2][colIndex] = "ocupado";
+                tabla.value[rowIndex + 2][colIndex] = "ocupado";
             }
         }
     })
